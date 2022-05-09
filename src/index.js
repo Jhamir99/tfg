fd91718a9da626e27ee9ba52152a92f9eafdfb99
Jhamir99 <jhamirjohan@gmail.com>
2022-05-09 22:16:44 +0200

    Nav + language selector

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend'
+import 'bootstrap/dist/js/bootstrap.js'
+
+//import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'flag-icon-css/css/flag-icons.min.css'
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    supportedLngs: ['en', 'es', 'ca'],
+    fallbackLng: "en",
+    detection: {
+      order: ['cookie', 'localStorage', 'sessionStorage', 'navigator', 'htmlTag', 'path', 'subdomain', 'querystring',],
+      caches: ['localStorage', 'cookie']
+    },
+    backend: {
+      loadPath: '/assets/lang/{{lng}}.json'
+    },
+    react: {
+      useSuspense: false
+    }
+  });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
